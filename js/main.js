076883aabc932f46e996c3ea350ae8538c9c4a99
metwse <108795071+metwse@users.noreply.github.com>
2022-07-08 19:47:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ for (x = 0; x < pages_list.length; x++) { pages[pages_list[x].id] = pages_list[x
 
 function page(page_name) { for (x in pages_in_str) { pages[pages_in_str[x]].style.display = ["none", "block"][(page_name == pages_in_str[x]) + 1 - 1] } }
 function loading(state) { document.getElementById("loading").style.display = ["none", "block"][state + 1 - 1] }
-function windowState(title, path, description = "") { document.getElementById("title").innerHTML = title; document.getElementById("og-title").content = title; document.getElementById("og-description").content = description; window.history.pushState({ "pageTitle": title }, title, url + path) }
+function windowState(title, path, description = "") { gtag('set', 'page_path', path); gtag('event', 'page_view'); document.getElementById("title").innerHTML = title; document.getElementById("og-title").content = title; document.getElementById("og-description").content = description; window.history.pushState({ "pageTitle": title }, title, url + path) }
 function captcha(state = 0, callback = null) {
     if (state == 0) {
         loading(1)
@@ -142,7 +142,7 @@ for (let id = 0; id < articles.length; id++) {
 function loadUri() {
     let uri = decodeURI(window.location.search).split("/")
     if (uri.length == 1) { uri = decodeURI(window.location.pathname).split("/"); uri.shift() }
-    else { uri.shift(); }
+    else { uri.shift() }
     while (uri[0] == "") { uri.shift() }
     if (uri[0] == undefined) { homepage() }
     else if (uri[0] == "k" && uri.length == 2) { articlesList(uri[1]) }
