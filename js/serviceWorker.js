a855ffa998a94261fe1eb78473a7ce87ddc9e3cd
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 02:49:03 +0300

    pwa

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,31 +1,50 @@
-﻿var RUNTIME = 'runtime-1';
+﻿var VERSION = 'version_002';
+var URLS = [
+    "/",
+    "/404.html",
+    "/index.html",
+    "/css/styles.css",
+    "/img/icon.png",
+    "/js/app.js"
+]
 
-self.addEventListener('activate', event => {
-    const currentCaches = [RUNTIME];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }));
-        }).then(() => self.clients.claim())
-    );
-});
+self.addEventListener('fetch', function (e) {
+    console.log('Fetch request : ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Responding with cache : ' + e.request.url);
+                return request
+            } else {
+                console.log('File is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+        })
+    )
+})
 
-self.addEventListener('fetch', function (event) {
-    var req = event.request.clone();
-    if (req.clone().method == "GET") {
-        event.respondWith(
-            caches.open(RUNTIME).then(function (cache) {
-                return cache.match(event.request).then(function (response) {
-                    var fetchPromise = fetch(event.request).then(function (networkResponse) {
-                        cache.put(event.request, networkResponse.clone());
-                        return networkResponse;
-                    })
-                    return response || fetchPromise;
-                })
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(VERSION).then(function (cache) {
+            console.log('Installing cache : ' + VERSION);
+            return cache.addAll(URLS)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            var cacheWhitelist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
             })
-        );
-    }
-});
+            cacheWhitelist.push(VERSION);
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheWhitelist.indexOf(key) === -1) {
+                    console.log('Deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
