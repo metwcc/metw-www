b291dd082b8cffb69729cf8eb381237fea923831
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 01:26:15 +0300

    pwa

--- js/main.js
+++ js/main.js
@@ -178,3 +178,5 @@ function loadUri() {
 
 document.getElementById("article-send-comment-name").value = localStorage.getItem("name"); loadUri()
 window.onpopstate = function (event) { disableStateUpdates = true; loadUri() }
+
+if ("serviceWorker" in navigator) { window.addEventListener("load", function () { navigator.serviceWorker.register("/serviceWorker.js").then(res => console.log("service worker registered")).catch(err => console.log("service worker not registered", err)) }) }
---
+++ js/pwaServiceWorker.js
@@ -0,0 +1,29 @@
+﻿const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/css/main.css",
+    "/css/loading.css",
+    "/js/main.js",
+    "/icons/icon-32.jpg",
+    "/icons/icon-64.jpg",
+    "/icons/icon-128.jpg",
+    "/icons/icon-256.jpg",
+    "/icons/icon-512.jpg"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
