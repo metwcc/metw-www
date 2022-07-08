c94bcba56fa430c15734bf550ca14f202bbcbb19
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 02:38:17 +0300

    Update serviceWorker.js

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,18 +1,31 @@
-﻿const static = "metw-cc-v1"
-const assets = [
-    "/",
-    "/?/",
-    "/css/main.css",
-    "/css/loading.css",
-    "/js/main.js",
-    "/images/logo/circle/32.png",
-    "/images/logo/circle/48.png",
-    "/images/logo/circle/72.png",
-    "/images/logo/circle/128.png",
-    "/images/logo/circle/256.png",
-    "/images/logo/circle/512.png",
-    "/images/logo/maskable/512.png"
-]
+﻿var RUNTIME = 'runtime-1';
 
-self.addEventListener("install", event => { event.waitUntil(precache()); }); function precache() { return caches.open(static).then(cache => cache.addAll(assets)); }
-self.addEventListener("fetch", event => { const request = event.request; if (request.method !== "GET") { return; } })
+self.addEventListener('activate', event => {
+    const currentCaches = [RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    var req = event.request.clone();
+    if (req.clone().method == "GET") {
+        event.respondWith(
+            caches.open(RUNTIME).then(function (cache) {
+                return cache.match(event.request).then(function (response) {
+                    var fetchPromise = fetch(event.request).then(function (networkResponse) {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    })
+                    return response || fetchPromise;
+                })
+            })
+        );
+    }
+});
