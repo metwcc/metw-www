9c4d401a6e72285e8386cca083955319b44184d1
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 01:32:10 +0300

    Update pwaServiceWorker.js

--- js/pwaServiceWorker.js
+++ js/pwaServiceWorker.js
@@ -1,4 +1,4 @@
-﻿const staticDevCoffee = "dev-coffee-site-v1"
+﻿const static = "metw-cc-v1"
 const assets = [
     "/",
     "/index.html",
@@ -14,7 +14,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
     installEvent.waitUntil(
-        caches.open(staticDevCoffee).then(cache => {
+        caches.open(static).then(cache => {
             cache.addAll(assets)
         })
     )
