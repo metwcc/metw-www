06a25c31cf8b24fa00ad0ad3f3e47427499ad810
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 03:17:47 +0300

    pwa

---
+++ js/serviceWorker.js
@@ -0,0 +1,13 @@
+﻿const static = "metw-cc-v2"
+const assets = [
+    "/",
+    "/?/",
+    "/css/main.css",
+    "/css/loading.css",
+    "/js/main.js",
+    "/images/logo/circle/192.png",
+    "/images/logo/maskable/512.png"
+]
+
+self.addEventListener("install", event => { event.waitUntil(precache()); }); function precache() { return caches.open(static).then(cache => cache.addAll(assets)); }
+self.addEventListener("fetch", event => { const request = event.request; if (request.method !== "GET") { return; } })
