2b61086cc1292e35ed265b7663d418ed93ad0a46
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 12:32:36 +0300

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,6 @@
 ﻿const static = "metw-cc-v1"
 const assets = [
     "/",
-    "/?/",
     "/css/main.css",
     "/css/loading.css",
     "/js/main.js",
@@ -15,4 +14,22 @@ const assets = [
 ]
 
 self.addEventListener("install", event => { event.waitUntil(precache()); }); function precache() { return caches.open(static).then(cache => cache.addAll(assets)); }
-self.addEventListener("fetch", event => { const request = event.request; if (request.method !== "GET") { return; } })
+
+self.addEventListener("fetch", event => {
+    const request = event.request; if (request.method !== "GET") { return; }
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResponse = await event.preloadResponse;
+                    if (preloadResponse) { return preloadResponse; }
+                    const networkResponse = await fetch(event.request);
+                    return networkResponse;
+                } catch (error) {
+                    const cache = await caches.open(static);
+                    const cachedResponse = await cache.match("/");
+                    return cachedResponse;
+                }
+            })()
+        );
+})
