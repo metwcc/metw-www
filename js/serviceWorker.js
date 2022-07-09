d12c25ee9665ffb4e5fbbf38344b16a867cace39
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 03:01:28 +0300

    Update serviceWorker.js

--- js/serviceWorker.js
+++ js/serviceWorker.js
@@ -1,50 +1,18 @@
-﻿var VERSION = 'version_002';
-var URLS = [
+﻿const static = "metw-cc-v1"
+const assets = [
     "/",
-    "/404.html",
-    "/index.html",
-    "/css/styles.css",
-    "/img/icon.png",
-    "/js/app.js"
+    "/?/",
+    "/css/main.css",
+    "/css/loading.css",
+    "/js/main.js",
+    "/images/logo/circle/32.png",
+    "/images/logo/circle/48.png",
+    "/images/logo/circle/72.png",
+    "/images/logo/circle/128.png",
+    "/images/logo/circle/256.png",
+    "/images/logo/circle/512.png",
+    "/images/logo/maskable/512.png"
 ]
 
-self.addEventListener('fetch', function (e) {
-    console.log('Fetch request : ' + e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('Responding with cache : ' + e.request.url);
-                return request
-            } else {
-                console.log('File is not cached, fetching : ' + e.request.url);
-                return fetch(e.request)
-            }
-        })
-    )
-})
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(VERSION).then(function (cache) {
-            console.log('Installing cache : ' + VERSION);
-            return cache.addAll(URLS)
-        })
-    )
-})
-
-self.addEventListener('activate', function (e) {
-    e.waitUntil(
-        caches.keys().then(function (keyList) {
-            var cacheWhitelist = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX)
-            })
-            cacheWhitelist.push(VERSION);
-            return Promise.all(keyList.map(function (key, i) {
-                if (cacheWhitelist.indexOf(key) === -1) {
-                    console.log('Deleting cache : ' + keyList[i]);
-                    return caches.delete(keyList[i])
-                }
-            }))
-        })
-    )
-})
+self.addEventListener("install", event => { event.waitUntil(precache()); }); function precache() { return caches.open(static).then(cache => cache.addAll(assets)); }
+self.addEventListener("fetch", event => { const request = event.request; if (request.method !== "GET") { return; } })
