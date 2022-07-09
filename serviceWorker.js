a6d78d98ba65f87cfc04f33e84c04df1c4fa97ed
metwse <108795071+metwse@users.noreply.github.com>
2022-07-09 13:12:39 +0300

    offline page

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,6 @@
 ﻿const static = "metw-cc-v1"
 const assets = [
     "/offline.html",
-    "https://code.jquery.com/jquery-3.4.1.min.js",
-    "https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap",
-    "https://fonts.googleapis.com", "https://fonts.gstatic.com"
 ]
 
 self.addEventListener("install", event => { event.waitUntil(precache()); }); function precache() { return caches.open(static).then(cache => cache.addAll(assets)); }
