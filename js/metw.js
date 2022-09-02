e2a22224631762119fb55cd40c6f00316f50e0dd
metwse <108795071+metwse@users.noreply.github.com>
2022-09-02 09:05:40 +0300

    Update metw.js

--- js/metw.js
+++ js/metw.js
@@ -17,7 +17,7 @@ class Session {
             if (options.json) body = JSON.stringify(options.json), headers['content-type'] = 'application/json', options.method = 'post'
             if (this.SID) headers.SID = this.SID
             var raw, ok, res = await fetch(url.backend + options.path, { method: options.method || 'get', headers: headers, body: body, timeout: Number.POSITIVE_INFINITY })
-                .then(res => { ok = res.ok, raw = res; return res.json() }).then(json => [json, ok, raw])
+                .then(res => { ok = res.ok, raw = res; return res.json() }).catch(() => window.location.replace('/offline.html')).then(json => [json, ok, raw])
             if (res[2].status == 401) this.disconnect()
             if (res[2].status == 429 && options.retry !== false) {
                 setTimeout(async () => resolve(await this.request(options)), (parseInt(res[2].headers.get('RateLimit-Reset'))) * 1000);
