9e8ab86beb218b36c652f93db2df1ef7ee552307
metwse <108795071+metwse@users.noreply.github.com>
2022-09-02 09:01:35 +0300

    Update metw.js

--- js/metw.js
+++ js/metw.js
@@ -16,7 +16,7 @@ class Session {
             if (options.headers) headers = { ...headers, ...options.headers }
             if (options.json) body = JSON.stringify(options.json), headers['content-type'] = 'application/json', options.method = 'post'
             if (this.SID) headers.SID = this.SID
-            var raw, ok, res = await fetch(url.backend + options.path, { method: options.method || 'get', headers: headers, body: body })
+            var raw, ok, res = await fetch(url.backend + options.path, { method: options.method || 'get', headers: headers, body: body, timeout: Number.POSITIVE_INFINITY })
                 .then(res => { ok = res.ok, raw = res; return res.json() }).then(json => [json, ok, raw])
             if (res[2].status == 401) this.disconnect()
             if (res[2].status == 429 && options.retry !== false) {
